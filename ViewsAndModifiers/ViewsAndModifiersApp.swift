2c086de8af777617bcdbe8da068c976ef231e55d
prosvetovvv <vp12@yandex.ru>
2021-02-15 18:12:09 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Vitaly Prosvetov on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
